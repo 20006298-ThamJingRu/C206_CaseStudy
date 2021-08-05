6a91d270871914cca0c9b0de80a2bd00a6157645
20029916 <20029916@pritzpc>
2021-08-05 22:32:09 +0800

    main program

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -1,8 +1,15 @@
+import java.util.ArrayList;
 
 public class C206_CaseStudy {
+	
+	private static ArrayList<Menu> menuList = new ArrayList<Menu>();
+	private static ArrayList<Account> accounts = new ArrayList<Account>();
+	
+	private static final int OPTION_OUT = 5;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 
 	}
 
