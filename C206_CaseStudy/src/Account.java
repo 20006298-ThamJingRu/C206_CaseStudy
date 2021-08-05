b200c104a0753f7e9e599e0a9301126127548427
20029916 <20029916@pritzpc>
2021-08-05 22:32:29 +0800

    created an account class

---
+++ C206_CaseStudy/src/Account.java
@@ -0,0 +1,13 @@
+
+public class Account {
+	private String username;
+	public String password;
+	private String role;
+	
+	public Account(String username, String password, String role) {
+		this.username = username;
+		this.password = password;
+		this.role = role;
+	}
+
+}
